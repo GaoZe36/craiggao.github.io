22a9be4d419b6731dfd0eef3b5c232eed74a1ea9
GaoZe36 <77998208+GaoZe36@users.noreply.github.com>
2021-01-26 12:39:14 -0800

    Add files via upload

---
+++ ex8/script.js
@@ -0,0 +1,22 @@
+document.querySelector("#a").addEventListener("click",a);
+function a(){
+	document.querySelector("#output").innerHTML = "The iconic stone heads protruding from the ground on Easter Island are familiar to most, but many don't realize what lies beneath the surface. In the '10s, archaeologists studying the hundreds of stone statues on the Pacific Island excavated two of the figures, revealing full torsos, which measure as high as 33 feet.";
+	
+}
+document.querySelector("#b").addEventListener("click",b);
+function b(){
+	document.querySelector("#output").innerHTML = "Just as earth has earthquakes, the moon has you guessed it moonquakes. Less common and less intense than the shakes that happen here, moonquakes are believed by U.S. Geological Survey (USGS) scientists to occur due to tidal stresses connected to the distance between the Earth and the moon.<img src='image/moon-1859616_1920.jpg' alt='moon' id='moon'>";
+
+}
+document.querySelector("#c").addEventListener("click",c);
+function c(){
+	document.querySelector("#output").innerHTML = "Specifically, the hottest spot ever recorded on Earth is El Azizia, in Libya, where a temperature of 136 degrees Fahrenheit was recorded on Sept. 13, 1922. While hotter spots have likely occurred in other parts of the planet at other times, this is the most scorching temperature ever formally recorded by a weather station.";
+}
+document.querySelector("#d").addEventListener("click",d);
+function d(){
+	document.querySelector("#output").innerHTML = "This might explain why airplane food gets such a bad reputation. The elevation in an airplane can have a detrimental effect on our ability to taste things. According to a 2010 study conducted by Germany's Fraunhofer Institute for Building Physics, the dryness experienced at a high elevation as well as low pressure reduces the sensitivity of a person's taste buds to sweet and salty foods by about 30 percent. Add that dry cabin air affects our ability to smell, and our ability to taste is reduced further.";
+}
+document.querySelector("#e").addEventListener("click",e);
+function e(){
+	document.querySelector("#output").innerHTML = "When we breathe in and out of our nose during the day, one nostril does most of the work at a time, with the duties switching every several hours. This \"nasal cycle\" is dictated by the same autonomic nervous system that regulates heart rate, digestion, and other unconscious bodily functions and is the reason why when our nose gets stuffed up it does so one nostril at a time.";
+}
